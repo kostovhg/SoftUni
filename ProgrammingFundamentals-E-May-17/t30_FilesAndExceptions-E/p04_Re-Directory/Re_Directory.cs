99f43710ff2c30ed610c2d4b4e631707ba890326
kostovhg <kostovhg@gmail.com>
2017-07-27 07:21:44 +0300

    Added Re-Dictionary and аccompanying files

---
+++ ProgrammingFundamentals-E-May-17/t30_FilesAndExceptions-E/p04_Re-Directory/Re_Directory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p04_Re_Directory
+{
+    class Re_Directory
+    {
+        static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles(@"../../Resources/input");
+            string output = @"../../Resources/output/";
+            Directory.CreateDirectory(output);
+            foreach (string file in files)
+            {
+                string ext = file.Substring(file.LastIndexOf(".") + 1);
+                Directory.CreateDirectory(output + ext + "s");
+                string name = file.Substring(file.LastIndexOf("\\"));
+                Directory.Move(file, output + ext + "s\\" + name);
+            }
+        }
+    }
+}
