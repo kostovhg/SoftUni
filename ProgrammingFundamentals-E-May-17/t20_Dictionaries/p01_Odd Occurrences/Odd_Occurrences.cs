4fca958e8e319c2b7b239298434777619f49cc86
kostovhg <kostovhg@gmail.com>
2017-08-12 22:07:19 +0300

    initial push of problems from Dictionaryes Lab

---
+++ ProgrammingFundamentals-E-May-17/t20_Dictionaries/p01_Odd Occurrences/Odd_Occurrences.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace p01_Odd_Occurrences
+{
+    class Odd_Occurrences
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, byte> dic = TurnToDictionary(Console.ReadLine().ToLower().Split());
+            Console.WriteLine(string.Join(", ",
+                dic.Where(x => x.Value % 2 == 1)
+                .Select(x => x.Key)
+                .ToArray()
+                ));
+        }
+
+        private static Dictionary<string, byte> TurnToDictionary(string[] v)
+        {
+            Dictionary<string, byte> dic = new Dictionary<string, byte>();
+            foreach (string str in v)
+            {
+                if (!dic.ContainsKey(str.ToLower())) dic[str] = 0;
+                dic[str]++;
+            }
+            return dic;
+        }
+    }
+}
---
+++ ProgrammingFundamentals-E-May-17/t20_Dictionaries/p02_CountRealNumbers/CountRealNumbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace p02_CountRealNumbers
+{
+    class CountRealNumbers
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<double, byte> counts = TurnToDictionary(
+                Console.ReadLine().Split()
+                .Select(x => double.Parse(x))
+                .ToArray());
+            foreach (KeyValuePair<double, byte> num in counts)
+            {
+                Console.WriteLine("{0} -> {1}", num.Key, num.Value);
+            } 
+        }
+        private static SortedDictionary<double, byte> TurnToDictionary(double[] v)
+        {
+            SortedDictionary<double, byte> dic = new SortedDictionary<double, byte>();
+            foreach (double num in v)
+            {
+                if (!dic.ContainsKey(num)) dic[num] = 0;
+                dic[num]++;
+            }
+            return dic;
+        }
+    }
+}
