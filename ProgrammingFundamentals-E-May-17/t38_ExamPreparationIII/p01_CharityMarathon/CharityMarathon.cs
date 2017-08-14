7b64542aab4b05f028c2548a8a96bcc72e8082e6
kostovhg <kostovhg@gmail.com>
2017-08-14 18:44:08 +0300

    Commit first problem "Charity Marathon" from Exam Preparation III

---
+++ ProgrammingFundamentals-E-May-17/t38_ExamPreparationIII/p01_CharityMarathon/CharityMarathon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace p01_CharityMarathon
+{
+    class CharityMarathon
+    {
+        static void Main(string[] args)
+        {
+            ulong days = ulong.Parse(Console.ReadLine());
+            ulong count = ulong.Parse(Console.ReadLine());
+            ulong laps = ulong.Parse(Console.ReadLine());
+            ulong len = ulong.Parse(Console.ReadLine());
+            ulong cap = ulong.Parse(Console.ReadLine()) * days;
+            decimal money = decimal.Parse(Console.ReadLine());
+
+            decimal totalKm = (((count > cap) ? cap : count) * laps * len) / 1000.0M;
+            Console.WriteLine("Money raised: {0:F2}", money * totalKm);
+
+        }
+    }
+}
