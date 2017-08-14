5cec6a9e45fa4c53fbf7dc2c6238f23a86406e0d
kostovhg <kostovhg@gmail.com>
2017-08-15 01:54:03 +0300

    Roli - The coder, that takes only 80/100 with regexes. TODO: try with string manipulating

---
+++ ProgrammingFundamentals-E-May-17/t38_ExamPreparationIII/p04_Roli-The Coder/Roli_The_Coder.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace p04_Roli_The_Coder
+{
+    class Roli_The_Coder
+    {
+        class Event
+        {
+            public long ID { get; set; }
+            public string EventName { get; set; }
+            public SortedSet<string> Guests { get; set; }
+
+            public void AddGuest(string guestNames)
+            {
+                MatchCollection mc = Regex.Matches(guestNames, @"(@\S+)");
+                if (mc.Count < 1) return;
+                foreach(Match m in mc)
+                {
+                    this.Guests.Add(m.Groups[1].Value);
+                }
+            }
+
+            public static Event Parse(string input)
+            {
+                
+                Match m = new Regex(@"(?'id'[0-9]+) #(?'event'\S+)(?'names'.*)").Match(input);
+                if (!m.Success) return null;
+                var instance = new Event();
+                instance.ID = long.Parse(m.Groups["id"].Value);
+                instance.EventName = m.Groups["event"].Value;
+                instance.Guests = new SortedSet<string>
+                    (from string name in m.Groups["names"].Value
+                     .Split(new char[] { ' ', '@' }, StringSplitOptions.RemoveEmptyEntries)
+                     .ToArray()
+                     select name);
+                    return instance;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Dictionary<long, Event> events = new Dictionary<long, Event>();
+            string input;
+            while (!"Time for Code".Equals(input = Console.ReadLine()))
+            {
+                Event e = Event.Parse(input);
+                if (e == null) continue;
+                if (events.ContainsKey(e.ID))
+                {
+                    if (events[e.ID].EventName.Equals(e.EventName))
+                    {
+                        events[e.ID].AddGuest(input);
+                    }
+                }
+                else
+                {
+                    events.Add(e.ID, e);
+                }
+            }
+            foreach (Event e in events.Values
+                .OrderByDescending(e => e.Guests.Count))
+            {
+                Console.WriteLine($"{e.EventName} - {e.Guests.Count}");
+                foreach (string guest in e.Guests)
+                {
+                    Console.WriteLine($"@{guest}");
+                }
+            }
+        }
+    }
+}
