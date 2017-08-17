c9bd432f7283dc3ba219bdf6a647258aa6a7ec7b
kostovhg <kostovhg@gmail.com>
2017-08-18 00:39:29 +0300

    Changed a check if worm already exist to LINQ query

--- ProgrammingFundamentals-E-May-17/t42_Exam30April2017/p04_WormsWorldParty/WormsWorldParty.cs
+++ ProgrammingFundamentals-E-May-17/t42_Exam30April2017/p04_WormsWorldParty/WormsWorldParty.cs
@@ -10,36 +10,31 @@ namespace p04_WormsWorldParty
     {
         static void Main(string[] args)
         {
-            Dictionary<string, Dictionary<string, long>> teams = new Dictionary<string, Dictionary<string, long>>();
+            Dictionary<string, Dictionary<string, ulong>> teams = new Dictionary<string, Dictionary<string, ulong>>();
             string input;
             while (!"quit".Equals(input = Console.ReadLine()))
             {
                 string[] tokens = input.Split(new string[] { " -> " }, StringSplitOptions.RemoveEmptyEntries);
                 string name = tokens[0];
                 string team = tokens[1];
-                long score = long.Parse(tokens[2]);
+                ulong score = ulong.Parse(tokens[2]);
+                // check for existing player shoudl be done before adding teams!!!
+                if (teams.Values.Any(x => x.ContainsKey(name))) continue;
                 if (!teams.ContainsKey(team))
                 {
-                    teams[team] = new Dictionary<string, long>();
-                    teams[team][name] = score;
+                    teams.Add(team, new Dictionary<string, ulong>());
                 }
-                else if(teams.Values.Any(x => x.ContainsKey(name)))
-                {
-                    continue;
-                }
-                else
-                {
-                    teams[team][name] = score;
-                }
+                teams[team].Add(name, 0);
+                teams[team][name] += score;
             }
             byte counter = 1;
-            foreach (KeyValuePair<string, Dictionary<string, long>> team in teams
-                .OrderByDescending(x => x.Value.Values.Sum())
-                .ThenByDescending(x => x.Value.Values.Average()))
+            foreach (KeyValuePair<string, Dictionary<string, ulong>> team in teams
+                .OrderByDescending(x => x.Value.Values.Sum(e => (decimal)e))
+                .ThenByDescending(x => x.Value.Values.Average(e => (decimal)e)))
             {
-                Console.WriteLine("{0}. Team: {1} – {2}", 
-                    counter, team.Key, team.Value.Sum(x => x.Value));
-                foreach (KeyValuePair<string, long> player in team.Value.OrderByDescending(x => x.Value))
+                Console.WriteLine("{0}. Team: {1} - {2}", 
+                    counter, team.Key, team.Value.Sum(x => (decimal)x.Value));
+                foreach (KeyValuePair<string, ulong> player in team.Value.OrderByDescending(x => x.Value))
                 {
                     Console.WriteLine("###{0} : {1}", player.Key, player.Value);
                 }
