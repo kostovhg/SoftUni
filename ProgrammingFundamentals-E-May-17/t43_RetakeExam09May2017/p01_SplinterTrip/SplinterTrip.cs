0c9c9a8e39d5c34515705242acb99bf22670f82b
kostovhg <kostovhg@gmail.com>
2017-08-18 10:17:05 +0300

    first two problems from "Programming fundamentals extended - retake exam 09 May 2017"

---
+++ ProgrammingFundamentals-E-May-17/t43_RetakeExam09May2017/p01_SplinterTrip/SplinterTrip.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace p01_SplinterTrip
+{
+    class SplinterTrip
+    {
+        static void Main(string[] args)
+        {
+            double dist = double.Parse(Console.ReadLine());
+            double tank = double.Parse(Console.ReadLine());
+            double windy = double.Parse(Console.ReadLine());
+            double fuel = ((dist - windy) * 25 + (windy) * (25 * 1.5)) * 1.05;
+            Console.WriteLine($"Fuel needed: {fuel:F}L");
+            if (fuel <= tank)
+            {
+                Console.WriteLine($"Enough with {(tank - fuel):F}L to spare!");
+            }
+            else
+            {
+                Console.WriteLine($"We need {(fuel - tank):F}L more fuel.");
+            }
+
+        }
+    }
+}
---
+++ ProgrammingFundamentals-E-May-17/t43_RetakeExam09May2017/p02_SpyGram/SpyGram.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace p02_SpyGram
+{
+    class SpyGram
+    {
+        static void Main(string[] args)
+        {
+            int[] key = Console.ReadLine().Select(x => int.Parse(x.ToString())).ToArray();
+            string input;
+            List<string[]> msgs = new List<string[]>();
+            Regex regex = new Regex(@"^TO: (?'recip'[A-Z]+); MESSAGE:.+;$");
+            while (!"END".Equals(input = Console.ReadLine()))
+            {
+                Match m = regex.Match(input);
+                if (!m.Success) continue;
+                msgs.Add(new string[] { m.Groups["recip"].Value, DecodeMsg(m.Value, key) });
+            }
+            foreach (string[] msg in msgs.OrderBy(x => x[0]))
+            {
+                Console.WriteLine(msg[1]);
+            }
+        }
+        
+
+        private static string DecodeMsg(string value, int[] key)
+        {
+            char[] decoded = new char[value.Length];
+            for (int i = 0; i < value.Length; i++)
+            {
+                decoded[i] = (char)(value[i] + key[i % (key.Length)]);
+            }
+            return new string(decoded);
+        }
+    }
+}
