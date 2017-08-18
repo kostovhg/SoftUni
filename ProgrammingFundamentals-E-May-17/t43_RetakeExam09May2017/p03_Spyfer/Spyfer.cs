abc5abd5261c43cf1d504f6cac274fecdc8fbfc5
kostovhg <kostovhg@gmail.com>
2017-08-18 13:24:00 +0300

    all tasks from 09 May exam. Changes in Spy Gram

---
+++ ProgrammingFundamentals-E-May-17/t43_RetakeExam09May2017/p03_Spyfer/Spyfer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace p03_Spyfer
+{
+    class Spyfer
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine()
+                .Split().Select(int.Parse)
+                .ToList();
+            for (int i = 0; i < nums.Count; )
+            {
+                int borders = ((i == 0) ? 0 : nums[i - 1]) + ((i == nums.Count - 1) ? 0 : nums[i + 1]);
+                if (nums[i] == borders)
+                {
+                    if (i < nums.Count - 1) nums.RemoveAt(i + 1);
+                    if (i > 0 ) nums.RemoveAt(i - 1);
+                    i = 0;
+                }
+                else i++;
+            }
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
---
+++ ProgrammingFundamentals-E-May-17/t43_RetakeExam09May2017/p04_NSA/NSA.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p04_NSA
+{
+    class NSA
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> countries = new Dictionary<string, Dictionary<string, int>>();
+            string input;
+            while (!"quit".Equals(input = Console.ReadLine()))
+            {
+                string[] tokens = input.Split(new string[] { " -> " }, StringSplitOptions.RemoveEmptyEntries);
+                string country = tokens[0];
+                string spy = tokens[1];
+                int days = int.Parse(tokens[2]);
+
+                if (!countries.ContainsKey(country)) countries.Add(country, new Dictionary<string, int>());
+                if (!countries[country].ContainsKey(spy)) countries[country].Add(spy, 0);
+                countries[country][spy] = days;
+            }
+            foreach (var country in countries.OrderByDescending(x => x.Value.Count))
+            {
+                Console.WriteLine($"Country: {country.Key}");
+                foreach (var spy in country.Value.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"**{spy.Key} : {spy.Value}");
+                }
+            }
+        }
+    }
+}
