760ca492ddcf0758409e988f0e7ba4080ac96a1c
kostovhg <kostovhg@gmail.com>
2017-08-29 14:04:56 +0300

    Adding controller (functionality for "create" view)

--- SoftwareTechnologies/Java-TODO-List/src/main/java/softuni/todolist/controller/TodoControler.java
+++ SoftwareTechnologies/Java-TODO-List/src/main/java/softuni/todolist/controller/TodoControler.java
@@ -2,9 +2,15 @@ package softuni.todolist.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import softuni.todolist.bindingModel.TodoBindingModel;
+import softuni.todolist.entity.Todo;
+import softuni.todolist.entity.User;
 import softuni.todolist.repository.TodoRepository;
 import softuni.todolist.repository.UserRepository;
 
@@ -23,5 +29,24 @@ public class TodoControler {
         model.addAttribute("view", "todo/create");
 
         return "base-layout";
+    }
+
+    @PostMapping("/todo/create")
+    @PreAuthorize("isAuthenticated()")
+    public String createProcess(TodoBindingModel todoBindingModel){
+        UserDetails user = (UserDetails) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+
+        User userEntity = this.userRepository.findByEmail(user.getUsername());
+
+        Todo todoEntity = new Todo(
+                todoBindingModel.getTitle(),
+                todoBindingModel.getContent(),
+                userEntity
+        );
+
+        this.todoRepository.saveAndFlush(todoEntity);
+
+        return "redirect:/";
     }
 }
