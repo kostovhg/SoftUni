aee72d7bebfdf50cfa493d49bd6c22ec61bd2bad
kostovhg <kostovhg@gmail.com>
2017-10-09 11:15:01 +0300

    Passed judge
    Rotating only indexes. First loop change each index till indexes count is reacherd.
    Second loop check if for specific initial index, we have complete cycle.
    If not, break and go to outer loop.

--- Java Fundamentals-Sep17/Java Advanced/t05_ObjectClassesAndCollections_E/src/p04_TruckTour.java
+++ Java Fundamentals-Sep17/Java Advanced/t05_ObjectClassesAndCollections_E/src/p04_TruckTour.java
@@ -1,10 +1,42 @@
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class p04_TruckTour {
-    private static Scanner sc = new Scanner(System.in);
+    private static ArrayDeque<Integer> indexes;
 
     public static void main(String[] args) {
+        Scanner scann = new Scanner(System.in);
+        int rows = Integer.parseInt(scann.nextLine());
 
+        indexes = new ArrayDeque<>();
+        Long[] pumps = new Long[rows];
+        Long[] distance = new Long[rows];
+
+        for (int i = 0; i < rows; i++) {
+            String[] input = scann.nextLine().split("\\s+");
+            pumps[i] = (Long.parseLong(input[0]));
+            distance[i] = (Long.parseLong(input[1]));
+            indexes.add(i);
+        }
+
+        for (int index = 0; index < rows; index++) {
+            boolean cycle = true;
+            int cIndex = indexes.poll();
+            indexes.add(cIndex);
+            Long fuel = 0L;
+            for (int i = 0; i <= rows; i++) {
+                fuel += pumps[(i + cIndex) % rows];
+                fuel -= distance[(i + cIndex) % rows];
+                if(fuel < 0){
+                    cycle = false;
+                    break;
+                }
+            }
+            if (cycle){
+                System.out.println(cIndex);
+                return;
+            }
+        }
 
     }
 }
