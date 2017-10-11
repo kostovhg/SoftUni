1d54ac873335a5af162d6498465cf3c2dc6341b0
kostovhg <kostovhg@gmail.com>
2017-10-11 14:35:59 +0300

    Passed

--- Java Fundamentals-Sep17/Java Advanced/t06_FunctionalProgramming_E/src/p02_KingsOfHonor.java
+++ Java Fundamentals-Sep17/Java Advanced/t06_FunctionalProgramming_E/src/p02_KingsOfHonor.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 import java.util.function.Consumer;
 
 public class p02_KingsOfHonor {
-    private static Scanner scann = new Scanner(System.in);
     private static Consumer<String> print = x -> System.out.printf("Sir %s%n", x);
     private static List<String> list = new ArrayList<>();
     private static Consumer<String> fill = x -> list.addAll(Arrays.asList(x.split("\\s+")));
 
     public static void main(String[] args) {
-        fill.accept(scann.nextLine());
+        fill.accept(new Scanner(System.in).nextLine());
 
         for (String str :
                 list) {
