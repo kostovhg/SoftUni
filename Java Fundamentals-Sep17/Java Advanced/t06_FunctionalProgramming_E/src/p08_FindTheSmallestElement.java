977590744b924eba1b80f31526850731cbe13db6
kostovhg <kostovhg@gmail.com>
2017-10-11 22:07:52 +0300

    Passed

--- Java Fundamentals-Sep17/Java Advanced/t06_FunctionalProgramming_E/src/p08_FindTheSmallestElement.java
+++ Java Fundamentals-Sep17/Java Advanced/t06_FunctionalProgramming_E/src/p08_FindTheSmallestElement.java
@@ -6,21 +6,20 @@ import java.util.stream.Collectors;
 
 public class p08_FindTheSmallestElement {
     private static Scanner sc = new Scanner(System.in);
+    private static Function<String, List<Integer>> readIntArr = x -> Arrays.
+            stream(x.split("\\s+"))
+            .map(Integer::valueOf).collect(Collectors.toList());
+    private static Function<List<Integer>, Integer> findSmallest = x -> {
+        int min = 0;
+        for (int i = 0; i < x.size() - 1; i++) {
+            if(x.get(i) >= x.get(i + 1)) min = i + 1;
+        }
+        return min;
+    };
+
 
     public static void main(String[] args) {
-        Function<String, List<Integer>> readIntArr = x -> Arrays.
-                stream(x.split("\\s+"))
-                    .map(Integer::valueOf).collect(Collectors.toList());
         List<Integer> arrList = readIntArr.apply(sc.nextLine());
-
-        Function<List<Integer>, Integer> findSmallest = x -> {
-            int min = 0;
-            for (int i = 0; i < x.size() - 1; i++) {
-                if(x.get(i) >= x.get(i + 1)) min = i + 1;
-            }
-            return min;
-        };
-
         System.out.println(findSmallest.apply(arrList));
     }
 }
