ae93c1ae05a0121349017a98ed5480e207120792
kostovhg <kostovhg@gmail.com>
2017-10-11 15:58:04 +0300

    Passed

---
+++ Java Fundamentals-Sep17/Java Advanced/t06_FunctionalProgramming_E/src/p04_FindEvensOrOdds.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class p04_FindEvensOrOdds {
+    private static Scanner scanner = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        String[] tokens = scanner.nextLine().split("\\s+");
+        Integer start = Integer.valueOf(tokens[0]);
+        Integer end = Integer.valueOf(tokens[1]);
+
+
+    }
+}
