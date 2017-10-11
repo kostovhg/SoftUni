b17c2fded3a38969a42fc5713721df556e893255
kostovhg <kostovhg@gmail.com>
2017-10-11 16:10:25 +0300

    Passed

--- Java Fundamentals-Sep17/Java Advanced/t06_FunctionalProgramming_E/src/p04_FindEvensOrOdds.java
+++ Java Fundamentals-Sep17/Java Advanced/t06_FunctionalProgramming_E/src/p04_FindEvensOrOdds.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.function.Predicate;
 
 public class p04_FindEvensOrOdds {
     private static Scanner scanner = new Scanner(System.in);
@@ -8,7 +9,24 @@ public class p04_FindEvensOrOdds {
         String[] tokens = scanner.nextLine().split("\\s+");
         Integer start = Integer.valueOf(tokens[0]);
         Integer end = Integer.valueOf(tokens[1]);
+        String condition = scanner.nextLine();
 
+        Predicate<Integer> tester = createTest(condition);
+        for (int i = start; i <= end; i++) {
+            if(tester.test(i)) {
+                System.out.printf("%d ", i);
+            }
+        }
 
+    }
+
+    private static Predicate<Integer> createTest(String condition) {
+        switch (condition){
+            case "odd":
+                return x -> x % 2 != 0;
+            case "even":
+                return x -> x % 2 == 0;
+        }
+        return x -> x % 3 == 1;
     }
 }
