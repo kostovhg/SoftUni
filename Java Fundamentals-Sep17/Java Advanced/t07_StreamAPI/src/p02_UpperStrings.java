a828328c0bb1090bb7e8e321f3aa592dafeab8db
kostovhg <kostovhg@gmail.com>
2017-10-13 08:52:05 +0300

    Passed

--- Java Fundamentals-Sep17/Java Advanced/t07_StreamAPI/src/p02_UpperStrings.java
+++ Java Fundamentals-Sep17/Java Advanced/t07_StreamAPI/src/p02_UpperStrings.java
@@ -1,12 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class p02_UpperStrings {
     private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
-
-
+        Arrays.stream(reader.readLine().split("\\s+"))
+                .map(String::toUpperCase)
+                .forEach(s -> System.out.print(s + " "));
     }
 }
