1ed2da015a6930e081faa209c16c15cd544fe93e
kostovhg <kostovhg@gmail.com>
2017-10-21 14:40:03 +0300

    exam task with HashMap<String, List<String>> and some sorting with substring from the nested list.

---
+++ Java Fundamentals-Sep17/Java Advanced/ExamPreparation/ExamPreparation_II/src/p04_FootbalStats.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class p04_FootbalStats {
+
+    private static HashMap<String, List<String>> stats = new HashMap<>();
+    private static Scanner scanner = new Scanner(System.in);
+    private static String splitter = "( - )|( )|(:)|(, )";
+
+    public static void main(String[] args) {
+        String line;
+        while(!"Season End".equals(line = scanner.nextLine())){
+            String[] tokens = line.split(splitter);
+            String firstTeam = tokens[0];
+            String secondTeam = tokens[1];
+            String firstTeamGoals = tokens[3];
+            String secondTeamGoals = tokens[4];
+
+            stats.putIfAbsent(firstTeam, new ArrayList<>());
+            stats.putIfAbsent(secondTeam, new ArrayList<>());
+            stats.get(firstTeam).add(secondTeam + " -> " + firstTeamGoals + ":" + secondTeamGoals);
+            stats.get(secondTeam).add(firstTeam + " -> " + secondTeamGoals + ":" + firstTeamGoals);
+        }
+        for (String team : scanner.nextLine().split(splitter)) {
+            stats.get(team).stream()
+                    .sorted(Comparator.comparing(x -> x.substring(0, x.indexOf(" "))))
+                    .forEach(o -> System.out.printf("%s - %s%n", team, o));
+        }
+    }
+}
