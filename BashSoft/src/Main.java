7800321d097831834db00a1a5342fc7c176d1bc7
kostovhg <kostovhg@gmail.com>
2017-10-25 11:47:02 +0300

    BashSoft: - rename main class to Main

--- BashSoft/src/Main.java
+++ BashSoft/src/Main.java
@@ -2,7 +2,7 @@ import IO.InputReader;
 
 import java.io.IOException;
 
-public class Program {
+public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         /*
         StudentRepository.initializeData();
