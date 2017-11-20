6b683aa8e941808b914c6721509ae8b3bf106f8c
kostovhg <kostovhg@gmail.com>
2017-11-20 14:42:10 +0200

    Java OOP Advanced - Iterators and Comparators - ComparableBook

---
+++ Java Fundamentals-Sep17/Java_OOP_Advanced/t03_IteratorsAndComparators/src/p03_ComparableBook/Book.java
@@ -0,0 +1,68 @@
+package p03_ComparableBook;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Book implements Comparable<Book>{
+
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public Book(String title, int year, String... authors) {
+        this.setTitle(title);
+        this.setYear(year);
+        this.setAuthors(authors);
+    }
+
+    private void setTitle(String title){
+        this.title = title;
+    }
+
+    private void setYear(int year) {
+        this.year = (year);
+    }
+
+    private void setAuthors(String... authors){
+        if(authors != null){
+            this.authors = new ArrayList<>(Arrays.asList(authors));
+            return;
+        }
+        this.authors = new ArrayList<>();
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public int getYear(){
+        return this.year;
+    }
+
+    public List<String> getAuthors() {
+        return Collections.unmodifiableList(this.authors);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", year=" + year +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Book o) {
+        int firstComparison = this.getTitle().compareTo(o.getTitle());
+       if(firstComparison == 0){
+           if(this.getYear() > o.getYear()) {return 1;}
+           else if(this.getYear() < o.getYear()) {return -1; }
+           return 0;
+       } else {
+           return firstComparison;
+       }
+    }
+}
