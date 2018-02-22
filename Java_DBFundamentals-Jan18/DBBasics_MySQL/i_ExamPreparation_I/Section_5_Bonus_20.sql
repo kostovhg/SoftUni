7c8ea4f702a455ac010a7409528b1a0080d5d166
kostovhg <kostovhg@gmail.com>
2018-02-22 16:26:31 +0200

    DB Basics - Exam Preparation I - problem 20

---
+++ Java_DBFundamentals-Jan18/DBBasics_MySQL/i_ExamPreparation_I/Section_5_Bonus_20.sql
@@ -0,0 +1,42 @@
+-- 20. Categories Revision
+SELECT 
+	c.`name`,
+    COUNT(r.id),
+    CASE
+		WHEN (
+			SELECT 
+				COUNT(*) 
+				FROM reports AS rr 
+				WHERE
+					rr.status_id = (SELECT id FROM `status` WHERE label = 'waiting') 
+					AND rr.category_id = c.id
+			) > (
+			SELECT
+				COUNT(*) 
+				FROM reports AS rr 
+				WHERE
+					rr.status_id = (SELECT id FROM `status` WHERE label = 'in progress') 
+					AND rr.category_id = c.id
+			) THEN 'waiting'
+				WHEN (
+			SELECT 
+				COUNT(*) 
+				FROM reports AS rr 
+				WHERE
+					rr.status_id = (SELECT id FROM `status` WHERE label = 'in progress') 
+					AND rr.category_id = c.id
+			) > (
+			SELECT
+				COUNT(*) 
+				FROM reports AS rr 
+				WHERE
+					rr.status_id = (SELECT id FROM `status` WHERE label = 'waiting') 
+					AND rr.category_id = c.id
+			) THEN 'in progress'
+		ELSE 'equal'
+	END as main_status
+FROM categories AS c
+JOIN reports AS r ON r.category_id = c.id
+JOIN `status` AS s ON r.status_id = s.id
+WHERE s.label IN ('waiting', 'in progress')
+GROUP BY c.`name`;
