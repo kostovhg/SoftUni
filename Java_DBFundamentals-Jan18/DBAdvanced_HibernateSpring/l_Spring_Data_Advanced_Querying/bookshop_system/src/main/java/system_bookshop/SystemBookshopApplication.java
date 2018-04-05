2989f93ee3b72753708457e455d47b05ccd51ef1
kostovhg <kostovhg@gmail.com>
2018-04-05 15:02:09 +0300

    DB Advanced - Spring Data Intro Advanced Querying - Exercise

--- Java_DBFundamentals-Jan18/DBAdvanced_HibernateSpring/l_Spring_Data_Advanced_Querying/bookshop_system/src/main/java/system_bookshop/SystemBookshopApplication.java
+++ Java_DBFundamentals-Jan18/DBAdvanced_HibernateSpring/l_Spring_Data_Advanced_Querying/bookshop_system/src/main/java/system_bookshop/SystemBookshopApplication.java
@@ -2,16 +2,14 @@ package system_bookshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
-//@PropertySource(value = {
-//        "classpath:application.properties",
-//        "classpath:application-local.properties",
-//        "classpath:params.properties"
-//})
 public class SystemBookshopApplication {
 
     public static void main(String[] args) {
+        // Using application-local.properties with spring.datasource.password set to local mysql password
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "local");
         SpringApplication.run(SystemBookshopApplication.class, args);
     }
 }
