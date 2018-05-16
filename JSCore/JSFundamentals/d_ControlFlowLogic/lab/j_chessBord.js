021541fec76591d5e1a8d812479ec982be2dd6f8
kostovhg <kostovhg@gmail.com>
2018-05-16 17:47:37 +0300

    JS Core - Fundamentals -> Lab: Operators And Logic Flow

--- JSCore/JSFundamentals/d_ControlFlowLogic/lab/j_chessBord.js
+++ JSCore/JSFundamentals/d_ControlFlowLogic/lab/j_chessBord.js
@@ -1,14 +1,12 @@
 function chessBord(n) {
     n = Math.floor(n);
     console.log(`<div class="chessboard">`);
-    let black = true;
+
     for (let i = 0; i < n; i++) {
         console.log(` <div>`);
         for (let j = 0; j < n; j++) {
-            console.log(`  <span class="${(black ? 'black' : 'white')}"></span>`);
-            black = !black;
+            console.log(`  <span class="${((i + j) % 2 ? 'white' : 'black')}"></span>`);
         }
-        i % 2 === 0 ? black = false : black = true;
         console.log(` </div>`);
     }
     console.log(`</div>`)
