5da8861295386fd30c7eab8bf2d899389adb457e
kostovhg <kostovhg@gmail.com>
2018-05-25 00:43:23 +0300

    JS Core - Fundamentals -> Lab: Arrays and Matrices

--- JSCore/JSFundamentals/f_ArraysAndMatrices/lab/a_SumFirstLast.js
+++ JSCore/JSFundamentals/f_ArraysAndMatrices/lab/a_SumFirstLast.js
@@ -1,8 +1,6 @@
 function sumFirstLast(arr) {
-
     arr = arr.map(x => Number(x));
     console.log(arr[0] + arr[arr.length - 1]);
-
 }
 
 sumFirstLast(['20', '30', '40']);
--- JSCore/JSFundamentals/f_ArraysAndMatrices/lab/d_FirstAndLastKNumbers.js
+++ JSCore/JSFundamentals/f_ArraysAndMatrices/lab/d_FirstAndLastKNumbers.js
@@ -2,7 +2,8 @@ function firstAndLastKNumbers(input) {
 
     let k = input.shift();
     console.log(input.slice(0, k).join(' '));
-    console.log(input.slice(input.length - k, input.length).join(' '));
+    //console.log(input.slice(input.length - k, input.length).join(' '));
+    console.log(input.slice(-k).join(' '));
 
 }
 
