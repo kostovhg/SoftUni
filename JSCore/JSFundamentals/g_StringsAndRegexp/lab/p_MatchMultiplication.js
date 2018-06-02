3ab6dc6c5edfe9e1d7ae9c30c923c8959f31c65f
kostovhg <kostovhg@gmail.com>
2018-06-02 14:21:55 +0300

    JS Core - Fundamentals -> Lab: Strings And Regexp: MatchMultiplication

--- JSCore/JSFundamentals/g_StringsAndRegexp/lab/p_MatchMultiplication.js
+++ JSCore/JSFundamentals/g_StringsAndRegexp/lab/p_MatchMultiplication.js
@@ -4,7 +4,8 @@ function matchMultiplication(input) {
         input
             .replace(
                 /(\-?[0-9]+)\s*\*\s*(\-?(?:\d+\.)?\d+)/g,
-                (m, p1, p2) => Number(p1) * Number(p2)));
+                (m, p1, p2) => +p1 * +p2));
 }
 
 matchMultiplication('My bill: 2*2.50 (beer); 2* 1.20 (kepab); -2  * 0.5 (deposit).');
+matchMultiplication('My bill is: 4 * 2.50 (beer); 12* 1.50 (kepab); 1  *4.50 (salad); 2  * -0.5 (deposit).');
