59390f816844126532788f57d35a58b78e7b2760
kostovhg <kostovhg@gmail.com>
2018-07-02 17:19:51 +0300

    A test

---
+++ JSCore/JSAdvanced/i_ObjectComposition/index.js
@@ -0,0 +1,11 @@
+let car = require('carFactory_');
+
+
+
+console.log(
+    car(
+        { model: 'VW Golf II',
+            power: 90,
+            color: 'blue',
+            carriage: 'hatchback',
+            wheelsize: 14 }));
---
+++ JSCore/JSAdvanced/i_ObjectComposition/test/test.js
@@ -0,0 +1,12 @@
+let carFactory_ = require('../c_CarFactory/carFactory_');
+
+
+
+
+console.log(
+    carFactory_(
+        { model: 'VW Golf II',
+            power: 90,
+            color: 'blue',
+            carriage: 'hatchback',
+            wheelsize: 14 }));
