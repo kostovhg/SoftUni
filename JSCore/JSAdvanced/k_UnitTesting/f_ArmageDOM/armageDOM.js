eba0fa5fb222cd173b155b00ffd4327901008127
kostovhg <kostovhg@gmail.com>
2018-07-05 16:35:57 +0300

    JSCore - Advanced -> Exercises: Unit Testing

--- JSCore/JSAdvanced/k_UnitTesting/f_ArmageDOM/armageDOM.js
+++ JSCore/JSAdvanced/k_UnitTesting/f_ArmageDOM/armageDOM.js
@@ -1,3 +1,9 @@
-function armageDOM(input){
-
+function nuke(selector1, selector2){
+    if(selector1 === selector2){
+        return;
+    }
+    $(selector1).filter(selector2).remove();
 }
+
+
+module.exports = nuke;
