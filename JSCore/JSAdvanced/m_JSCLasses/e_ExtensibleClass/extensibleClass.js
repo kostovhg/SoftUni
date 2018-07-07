09f85819969206b9afa699112ba207ed06226e48
kostovhg <kostovhg@gmail.com>
2018-07-07 11:20:21 +0300

    JSCore - Advanced -> Exercise: JavaScript Classes

--- JSCore/JSAdvanced/m_JSCLasses/e_ExtensibleClass/extensibleClass.js
+++ JSCore/JSAdvanced/m_JSCLasses/e_ExtensibleClass/extensibleClass.js
@@ -10,6 +10,7 @@ let result = (function () {
             for (let prop in template) {
                 ({}.toString.call(template[prop]) === '[object Function]') ?
                     Object.getPrototypeOf(this)[prop] = template[prop] :
+                    /*this.__proto__[prop] = template[prop] :*/
                     this[prop] = template[prop];
             }
         }
@@ -25,5 +26,8 @@ console.log(obj1.id);
 console.log(obj2.id);
 console.log(obj3.id);
 
-obj1.extend({extensionMethod: () => console.log('this is a method from template'), extensionProperty: 'Property from extension'});
+obj1.extend({extensionMethod: function() {console.log('this is a method from template')}, extensionProperty: 'Property from extension'});
+obj2.extend({extensionMethod2: function() { console.log('this is a method from template for obj2') }, extensionProperty2: 'Property from extension for obj2'});
 console.log(obj1);
+console.log(obj2);
+console.log(Object.getOwnPropertyNames(Object.getPrototypeOf(obj1)));
