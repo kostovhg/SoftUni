b49fa3efa3d5fddb503658d4ff7b4a2ec67e7639
kostovhg <kostovhg@gmail.com>
2018-07-12 19:09:18 +0300

    JSCore - Advanced => Exercises: Exam Preparation III - 18 Mar 2018, task 2

--- JSCore/JSAdvanced/s_ExamPreparation_III/b_PaymentPackage/test/paymentPackage_tests.js
+++ JSCore/JSAdvanced/s_ExamPreparation_III/b_PaymentPackage/test/paymentPackage_tests.js
@@ -1,20 +1,8 @@
 let expect = require('chai').expect;
 let PaymentPackage = require('../paymentPackage');
 
-// after many checks:
-// test    8        : toString should return string
-// test 5, 8, 9     : toString should return correct object representation
-// test 5, 8, 9, 10 : toString should return (inactive)
-// test    8, 9     : toString returns properly calculated values
-// test    8, 9, 10 : toString with change on VAT and inactive should return correct result
-// test    8,       : instantiated with two correct arguments not to thrown error
-// test    8,       : instantiated class should be constructor.name
-// test 1,          : new class should have property name and it should be a string
-// test 3, 5, 8     : new class should have property Vat, and it should be correct number
-
-
 describe('PaymentPackage Tests', function () {
-    let inputTypes = {
+    let iTypes = {
         emptyStr: '', date: new Date(), nothing: null, obj: {x: 1},
         zero: 0, negative: -5, floating: 5.5, positive: 50, bool: false,
         someString: 'Pesho'
@@ -23,89 +11,43 @@ describe('PaymentPackage Tests', function () {
     let obj;
 
     describe('Throw error for :', function () {
-        // gets test 1
-        it('name not non-empty string', () => {
-            let wrongNames = [inputTypes.emptyStr, inputTypes.date, inputTypes.nothing, inputTypes.obj, inputTypes.positive, inputTypes.bool];
-            wrongNames.forEach(x => expect(() => create(x, inputTypes.positive)).to.throw(Error));
+        it('name not being non-empty string', () => {
+            let wrongNames = [iTypes.emptyStr, iTypes.date, iTypes.nothing, iTypes.obj, iTypes.positive, iTypes.bool];
+            wrongNames.forEach(x => expect(() => create(x, iTypes.positive)).to.throw(Error)); // gets test 1
         });
-        // gets test 2
-        it('value not non-negative number', () => {
-            let wrongValues = [inputTypes.date, inputTypes.nothing, inputTypes.obj, inputTypes.bool, inputTypes.someString, inputTypes.negative];
-            wrongValues.forEach(x => expect(() => create(inputTypes.someString, x)).to.throw(Error));
 
+        it('values not being non-negative number', () => {
+            let wrongValues = [iTypes.date, iTypes.nothing, iTypes.obj, iTypes.bool, iTypes.someString, iTypes.negative];
+            wrongValues.forEach(x => expect(() => create(iTypes.someString, x)).to.throw(Error)); // gets test 2
         });
-    });
-    describe('toString to return', function () {
-        beforeEach('create obj', function () {
-            // ! Very important is to reinitialize the object before each test
-            obj = create(inputTypes.someString, inputTypes.positive);
-        });
-        // needed for tests 8 and 9
-        it('correct representation on inactive obj', function () {
-            obj.active = false;
-            expect(obj.toString()).to.equal('Package: Pesho (inactive)\n' +
-                '- Value (excl. VAT): 50\n' +
-                '- Value (VAT 20%): 60')
-        });
-        /* Not needed
-        it('correct representation on active obj', function () {
-                    expect(obj.toString()).to.equal('Package: Pesho\n' +
-                        '- Value (excl. VAT): 50\n' +
-                        '- Value (VAT 20%): 60');
-                });
-        it('changing the value should change value and VAT', function () {
-             obj.value = 100.40;
-             expect(obj.toString()).to.equal("Package: Pesho\n" +
-                 "- Value (excl. VAT): 100.4\n" +
-                 "- Value (VAT 20%): 120.48")
-         });
-        */
     });
     describe('Correct functioning', function () {
         beforeEach('create obj', function () {
-            // ! Very important is to reinitialize the object before each test
-            obj = create(inputTypes.someString, inputTypes.positive);
+            obj = create(iTypes.someString, iTypes.positive);
         });
-        /*  Not needed
-        it('instantiate creates object ', function () {
-            expect(obj).to.be.an('object');
-            expect(obj).to.be.instanceOf(PaymentPackage);
-        });
-        it('instance to have name property ', function () {
-            expect(obj).to.have.property('name', par.someString,).and.to.be.a('string');
-        });
-        it('instance to have value property ', function () {
-            expect(obj).to.have.property('value', par.positive).and.to.be.a('number');
-        });
-        it('instance to have constructor', function () {
-            expect(obj).to.have.property('constructor').to.be.a('function');
-        });
-        it('instance to be of type PaymentPackage', function () {
-            expect(obj).to.be.instanceOf(PaymentPackage);
-        });
-        it('constructor should accept 2 arguments', function () {
-            expect(PaymentPackage.length).to.be.equal(2)
-        });
-        */
-        it('instance to have VAT property ', function () {
+        it('instance and class to have VAT property (default: 20)', function () {
             expect(obj).to.have.property('VAT', 20).and.to.be.a('number'); // test 3
             expect(PaymentPackage.prototype.hasOwnProperty('VAT')).to.be.true; // test 5
 
         });
-        it('instance to have active property ', function () {
-            // tests 4 and 6
-            expect(obj).to.have.property('active', true).to.be.a('boolean');
-            expect(PaymentPackage.prototype.hasOwnProperty('active')).to.be.true;
-
+        it('instance and class to have active property (default: true)', function () {
+            expect(obj).to.have.property('active', true).and.to.be.a('boolean'); // test 6
+            expect(PaymentPackage.prototype.hasOwnProperty('active')).to.be.true; // test 4
         });
-        it("should change the value to 0", () => {
-            // solves test 7 !?
-            obj.value = 0;
-            expect(obj.value).to.be.equal(0);
+        it("set value should work with 0", () => {
+            obj.value = iTypes.zero;
+            expect(obj.value).to.be.equal(0); // solves test 7 !?
         });
-        it('instance to have toString function ', function () {
-            // solves test 8
-            expect(obj).to.have.property('toString').to.be.a('function');
+    });
+    describe('toString to return', function () {
+        // needed for tests 8 and 9
+        // test 8 does not pass if this check is not in separate describe block ?!?!?
+        it('correct toString representation on inactive obj', function () {
+            obj = create(iTypes.someString, iTypes.positive);
+            obj.active = false;
+            expect(obj.toString()).to.equal('Package: Pesho (inactive)\n' +
+                '- Value (excl. VAT): 50\n' +
+                '- Value (VAT 20%): 60')
         });
     });
 });
