a03d732cc0f97ebe558d5757f0a7564ade97a4e5
kostovhg <kostovhg@gmail.com>
2018-07-12 12:03:22 +0300

    JSCore - Advanced => Exercises: Exam Preparation - 18 Mar 2018 - task 3 (not finished)

--- JSCore/JSAdvanced/s_ExamPreparation_III/c_PaymentProcessor/paymentProcessor.js
+++ JSCore/JSAdvanced/s_ExamPreparation_III/c_PaymentProcessor/paymentProcessor.js
@@ -1,3 +1,56 @@
 class PaymentProcessor{
 
 }
+
+// Initialize processor with default options
+const generalPayments = new PaymentProcessor();
+generalPayments.registerPayment('0001', 'Microchips', 'product', 15000);
+generalPayments.registerPayment('01A3', 'Biopolymer', 'product', 23000);
+console.log(generalPayments.toString());
+
+// Should throw an error (invalid type)
+generalPayments.registerPayment('E028', 'Rare-earth elements', 'materials', 8000);
+
+generalPayments.setOptions({types: ['product', 'material']});
+generalPayments.registerPayment('E028', 'Rare-earth elements', 'material', 8000);
+console.log(generalPayments.get('E028'));
+generalPayments.registerPayment('CF15', 'Enzymes', 'material', 55000);
+
+// Should throw an error (ID not found)
+generalPayments.deletePayment('E027');
+// Should throw an error (ID not found)
+generalPayments.get('E027');
+
+generalPayments.deletePayment('E028');
+console.log(generalPayments.toString());
+
+// Initialize processor with custom types
+const servicePyaments = new PaymentProcessor({types: ['service']});
+servicePyaments.registerPayment('01', 'HR Consultation', 'service', 3000);
+servicePyaments.registerPayment('02', 'Discount', 'service', -1500);
+console.log(servicePyaments.toString());
+
+// Initialize processor with custom precision
+const transactionLog = new PaymentProcessor({precision: 5});
+transactionLog.registerPayment('b5af2d02-327e-4cbf', 'Interest', 'other', 0.00153);
+console.log(transactionLog.toString());
+
+/*
+Summary:
+- Payments: 2
+- Balance: 38000.00
+Details about payment ID: E028
+- Name: Rare-earth elements
+- Type: material
+- Value: 8000.00
+Summary:
+- Payments: 3
+- Balance: 93000.00
+Summary:
+- Payments: 2
+- Balance: 1500.00
+Summary:
+- Payments: 1
+- Balance: 0.00153
+
+ */
