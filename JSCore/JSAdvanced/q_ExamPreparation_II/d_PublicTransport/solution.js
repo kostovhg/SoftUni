4ac17b1af5d8ceed611bd97970bca48f5c9fc62e
kostovhg <kostovhg@gmail.com>
2018-07-13 21:47:52 +0300

    JSCore - Advanced => Exercises: Exam Preparation II - 18 Mar 2018

--- JSCore/JSAdvanced/q_ExamPreparation_II/d_PublicTransport/solution.js
+++ JSCore/JSAdvanced/q_ExamPreparation_II/d_PublicTransport/solution.js
@@ -7,66 +7,52 @@ class PublicTransportTable {
 
     set name(name) {
         this._name = name;
-        $('table>caption').text(`${name}'s Public Transport`)
+        $(`table>caption`).text(`${name}'s Public Transport`)
     }
 
     addVehicle(vehicle) {
-        $('.vehicles-info')
-            .append($('<tr class="hide-info">')
-                .append($('<td>').text(`${vehicle.type}`))
-                .append($('<td>').text(`${vehicle.name}`))
+        $(`.vehicles-info`)
+            .append($(`<tr class="hide-info"><td>${vehicle.type}</td><td>${vehicle.name}</td></tr>`)
                 .append($('<td>')
-                    .append($('<button>').text('More Info')
+                    .append($('<button>More Info</button>')
                         .on('click', (e) => {
                             let thatRow = $(e.target).parent().parent();
-                            $(e.target).text('Less Info');
                             if (thatRow.hasClass('hide-info')) {
-                                thatRow.attr('class', 'show-info');
-                                ($('<tr class="more-info">')
-                                    .append($('<td colspan="3">')
-                                        .append($('<table>')
-                                            .append($('<tr>').append($('<td>').text(`Route: ${vehicle.route}`)))
-                                            .append($('<tr>').append($('<td>').text(`Price: ${vehicle.price}`)))
-                                            .append($('<tr>').append($('<td>').text(`Driver: ${vehicle.driver}`)))
-                                        ))).insertAfter(thatRow)
+                                $(e.target).text('Less Info');
+                                thatRow.attr('class', 'show-info')
+                                    .after($(`<tr class="more-info"><td colspan="3">` +
+                                        `<table><tr><td>Route: ${vehicle.route}</td></tr>` +
+                                        `<tr><td>Price: ${vehicle.price}</td></tr>` +
+                                        `<tr><td>Driver: ${vehicle.driver}</td></tr>`))
                             } else {
                                 thatRow.attr('class', 'hide-info');
                                 $(e.target).text('More Info');
                                 thatRow.next().remove();
-                            }
-                        }))))
+                            }}))));
     }
 
     attachEventsListeners() {
         $('.search-btn').on('click', () => {
-            let [typeField, nameField] = [$('input[name=type]'), $('input[name=name]')];
-            let [type, name] = [typeField.val(), nameField.val()];
+            let [type, name] = [$('input[name=type]').val(), $('input[name=name]').val()];
             if (!type && !name) return null;
             $('.show-info, .hide-info').each(function () {
                 let that = $(this);
-                let [thatType, thatName, thatButton] = [
-                    that.find('td').eq(0).text(),
-                    that.find('td').eq(1).text(),
-                    that.find('button')[0]];
-                if (!thatType.includes(type) || !thatName.includes(name)) {
+                if (!that.find('td').eq(0).text().includes(type) || !that.find('td').eq(1).text().includes(name)) {
                     if (that.next().hasClass('more-info')) {
                         that.find('button')[0].click();
                     }
                     that.css('display', 'none');
-                    //console.log('Change type ' + thatType + ' name ' + thatName +' to hide')
                 } else {
                     that.css('display', '');
-                    //console.log('Change type ' + thatType + ' name ' + thatName +' to show')
                 }
             })
-        })
+        });
 
         $('.clear-btn').on('click', () => {
             $('input[name=type]').val('');
             $('input[name=name]').val('');
             $('.show-info, .hide-info').each(function () {
-                let that = $(this);
-                that.css('display', '');
+                $(this).css('display', '');
             })
         })
     }
