33e7bcb9bcda0444ae1c4974acc05ceaaa07fdf2
Hristo Kostov <kostovhg@gmail.com>
2018-07-18 16:30:14 +0300

    JavaScriptForFrondEnd => Lab: jQuery

--- FrontEnd/JavaScriptForFrontEnd/e_jQuery_lab/c_LinkButtons/link-buttons.js
+++ FrontEnd/JavaScriptForFrontEnd/e_jQuery_lab/c_LinkButtons/link-buttons.js
@@ -1,3 +1,21 @@
+// function attachEvents() {
+//     $('.button').on('click', function(event) {
+//         $('.button').each(b => $(this) === event.target ? $(b).addClass('selected') : $(b).removeClass('selected'));
+//    console.log('Clicked' + $(this).text())
+//     });
+// }
+
 function attachEvents() {
-   // TODO:
+    let [buttons, change] = [
+        $('a.button'),
+        (event) => {
+            buttons.each((b, t) =>
+                ($(event.target).text() === $(t).text()) ?
+                    $(t).addClass('selected') :
+                    $(t).removeClass('selected'))
+        }
+    ];
+
+    buttons.each((k, v) =>
+            $(v).on('click', change));
 }
--- FrontEnd/JavaScriptForFrontEnd/e_jQuery_lab/d_SelectableTowns/select-towns.js
+++ FrontEnd/JavaScriptForFrontEnd/e_jQuery_lab/d_SelectableTowns/select-towns.js
@@ -1,3 +1,21 @@
 function attachEvents() {
-	// TODO:
+    let [selected, background, items] = [
+        'data-selected',
+        'background-color',
+        $('#items li')
+    ];
+
+    items.on('click', (e) => {
+        let t = $(e.target);
+        (t.attr(selected)) ?
+            t.css(background,'').removeAttr(selected) :
+            t.css(background,'#DDD').attr(selected, true)
+    });
+
+    $('#showTownsButton').on('click', () => {
+        $('#selectedTowns').text([...items]
+            .filter(x => $(x).attr(selected))
+            .map(x => $(x).text())
+            .join(', '))
+    })
 }
